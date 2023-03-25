c23169fe82f177d45c937b0a3f93ac16f1ea233a
Oliver Joisten <o.joisten@live.se>
2023-03-25 17:12:00 +0100

    Code refactoring
    Cleaned up the following code:   src/App.js
                                                    src/components/FeedbackForm.jsx
                                                    src/components/FeedbackItem.jsx
                                                    src/components/FeedbackList.jsx
                                                    src/components/FeedbackStats.jsx
                                                    src/context/FeedbackContext.js

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
+import { useContext } from 'react'
 import FeedbackItem from './FeedbackItem'
-import PropTypes from 'prop-types'
+import FeedbackContext from '../context/FeedbackContext'
 
-function FeedbackList({ feedback, handleDelete } ) {
+function FeedbackList() {
+  const { feedback } = useContext ( FeedbackContext )
+
     if (!feedback || feedback.length === 0) {
         return <p>No Feedback Yet!</p>
   }
@@ -22,7 +25,6 @@ function FeedbackList({ feedback, handleDelete } ) {
             <FeedbackItem
               key={`item-${item.id}`}
               item={item}
-              handleDelete={handleDelete}
             />
             {/* <FeedbackItem key={item.id} item={item} /> */}
           </motion.div>
@@ -43,17 +45,6 @@ function FeedbackList({ feedback, handleDelete } ) {
   //     ))}
   //   </div>
   // )
-}
-
-FeedbackList.propTypes = {
-    feedback: PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.number.isRequired,
-            text: PropTypes.string.isRequired,
-            rating: PropTypes.number.isRequired,
-        })
-  ),
-  handleDelete: PropTypes.func.isRequired,
 }
 
 export default FeedbackList
